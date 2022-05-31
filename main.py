1ba2f46baf3aec549cd74f838b5babbf91162d1d
Superkid1235 <supakit.developer@gmail.com>
2022-05-31 21:59:52 +0700

    clear clear

---
+++ main.py
@@ -0,0 +1,21 @@
+class Customer():
+    def __init__(self,name, membership_type):
+        self.name = name
+        self.membership_type = membership_type
+
+    def update_membership(self, new_membership):    # not necessary
+        self.membership_type = new_membership
+
+
+c = Customer("Caleb",  "Gold")
+c2 = Customer("Brad", "Bronze")
+
+
+customer_list = [Customer("Caleb", "GOLD"),
+                 Customer("David", "BRONZE"),
+                 Customer("James", "SILVER")]
+
+
+print(f"Original membership of {customer_list[1].name} = {customer_list[1].membership_type}")
+customer_list[1].update_membership("GOLD")
+print(f"Upgraded membership of {customer_list[1].name} to >>  {customer_list[1].membership_type}")
