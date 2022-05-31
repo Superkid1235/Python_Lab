9c46566871a59f0660da28ea6259a4830d356bf1
Superkid1235 <supakit.developer@gmail.com>
2022-05-31 22:27:44 +0700

    String method __str__, how it is knvoked

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ class Customer():
         self.name = name
         self.membership_type = membership_type
 
-    # static method (not attached to an object)
+    # static method (invoke by a class itself, not an object of a class)
     def read_customer():
         print("this static method >> read_customer() has been invoked")
+
+    # string method __str__
+    def __str__(self):
+        return self.name + " " + self.membership_type
 
 # Customer Data Base
 customer_list = [Customer("Caleb", "GOLD"),
                  Customer("David", "BRONZE"),
                  Customer("James", "SILVER")]
 
-# invoking a static method read_customer():
-Customer.read_customer()
+# Invoking a __str__ method
+print(customer_list[0])             # try delete __str__ method to see different result
