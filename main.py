4986bddeff6ab4f7450b4aaafd958a1ce9925164
Superkid1235 <supakit.developer@gmail.com>
2022-05-31 22:04:07 +0700

    Create a List to store class objects

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ class Customer():
         self.name = name
         self.membership_type = membership_type
 
-    def update_membership(self, new_membership):    # not necessary
+    def update_membership(self, new_membership):
         self.membership_type = new_membership
 
 
 c = Customer("Caleb",  "Gold")
 c2 = Customer("Brad", "Bronze")
+
+
+customer_list = [Customer("Caleb", "GOLD"),
+                 Customer("David", "BRONZE"),
+                 Customer("James", "SILVER")]
+
 
 
 
